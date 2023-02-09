c10a2c431988868dc6910f83c61ae04901a7fa3a
Yamada-Hibiki <100133616+Yamada-Hibiki@users.noreply.github.com>
2023-02-09 10:39:15 +0100

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,12 +5,14 @@ function checkNotAuthenticated(req, res, next) {
   next();
 }
 
+
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect("/login");
 }
+
 
 module.exports = {
   checkNotAuthenticated,
